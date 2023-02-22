54e79603054ba56e3199e102fe4e4b24dd34a4f8
gyojincompany <luckyred@gmail.com>
2023-02-22 11:42:09 +0900

    MyBatis 설정 및 Dto 추가

---
+++ src/main/java/com/gyojincompany/home/dto/BoardDto.java
@@ -0,0 +1,75 @@
+package com.gyojincompany.home.dto;
+
+import java.sql.Timestamp;
+
+public class BoardDto {
+	
+	private int bid;//게시판 번호
+	private String btitle;//게시판 글 제목
+	private String bcontent;//게시판 글 내용
+	private int bhit;//조회수
+	private String bmid;//글쓴사람 아이디
+	private String bmname;//글쓴사람 이름
+	private Timestamp bdate;//글쓴 일시
+	
+	public BoardDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public BoardDto(int bid, String btitle, String bcontent, int bhit, String bmid, String bmname, Timestamp bdate) {
+		super();
+		this.bid = bid;
+		this.btitle = btitle;
+		this.bcontent = bcontent;
+		this.bhit = bhit;
+		this.bmid = bmid;
+		this.bmname = bmname;
+		this.bdate = bdate;
+	}
+	public int getBid() {
+		return bid;
+	}
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+	public String getBtitle() {
+		return btitle;
+	}
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+	public String getBcontent() {
+		return bcontent;
+	}
+	public void setBcontent(String bcontent) {
+		this.bcontent = bcontent;
+	}
+	public int getBhit() {
+		return bhit;
+	}
+	public void setBhit(int bhit) {
+		this.bhit = bhit;
+	}
+	public String getBmid() {
+		return bmid;
+	}
+	public void setBmid(String bmid) {
+		this.bmid = bmid;
+	}
+	public String getBmname() {
+		return bmname;
+	}
+	public void setBmname(String bmname) {
+		this.bmname = bmname;
+	}
+	public Timestamp getBdate() {
+		return bdate;
+	}
+	public void setBdate(Timestamp bdate) {
+		this.bdate = bdate;
+	}
+	
+	
+
+}
