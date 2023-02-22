ebcf85c4353fa3d294b054381a04371a83e40ad1
gyojincompany <luckyred@gmail.com>
2023-02-22 12:57:14 +0900

    회원 가입 기능 추가

---
+++ src/main/java/com/gyojincompany/home/controller/HomeController.java
@@ -0,0 +1,40 @@
+package com.gyojincompany.home.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gyojincompany.home.dao.Mapper;
+
+@Controller
+public class HomeController {
+	
+	@Autowired//의존주입(DI)
+	private SqlSession sqlSession;
+	
+	@RequestMapping(value = "/join")
+	public String join() {
+		
+		return "join";
+	}
+	
+	@RequestMapping(value = "/joinOk")
+	public String joinOk(HttpServletRequest request, Model model) {
+		
+		String mid = request.getParameter("mid");
+		String mpw = request.getParameter("mpw");
+		String mname = request.getParameter("mname");
+		String memail = request.getParameter("memail");
+		
+		Mapper dao = sqlSession.getMapper(Mapper.class);
+		dao.joinDao(mid, mpw, mname, memail);
+		model.addAttribute("mname", mname);
+		
+		return "joinOk";
+	}
+
+}
